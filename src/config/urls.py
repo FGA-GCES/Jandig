b12b3334c1ab89259a6f218f5107eaa78cee8948
caiozim112 <caioopua@gmail.com>
2023-05-23 16:10:23 -0300

    chore: fix do projeto

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,10 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import serve
 from django.contrib import admin
 from django.urls import include, path, re_path
-
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 urlpatterns = [
     path(settings.DJANGO_ADMIN_URL, admin.site.urls),
     path("", include("core.urls")),
+    path("favicon.ico",RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
     path("", include("core.routes")),
     path("users/", include("users.urls")),
     re_path("^docs/(?P<path>.*)$", serve, {"document_root": settings.DOCS_ROOT}),
